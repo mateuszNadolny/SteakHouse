9dc9bc7e4d87b4acd21374f219ea574adc92b520
mateuszNadolny <mateusznadolny00@gmail.com>
2022-07-14 21:26:33 +0200

    creating scss library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import './global_styles/index.css'
 
 function App() {
   return (
     <div className="App">
+      <p>test</p>
     </div>
-  );
+  ); 
 }
 
 export default App;
