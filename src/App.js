afd905834818cf6daa73b1b66dcce2eecb7639f4
mateuszNadolny <mateusznadolny00@gmail.com>
2022-07-20 15:37:50 +0200

    created infinite logo carousel

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import Navbar from './components/navbar/Navbar';
 import SimpleSwiper from './components/swiper/Swiper';
+import LogoCarousel from './components/logoCarousel/LogoCarousel';
 
 function App() {
   return (
     <>
       <Navbar />
       <SimpleSwiper />
-      <SimpleSwiper />
-      <SimpleSwiper />
-      <SimpleSwiper />
-      <SimpleSwiper />
-      <SimpleSwiper />
-      <SimpleSwiper />
+      <LogoCarousel />
     </>
   ); 
 }
---
+++ src/components/logoCarousel/LogoCarousel.js
@@ -0,0 +1,19 @@
+import "./../../global_styles/index.css";
+import styles from "./LogoCarousel.module.scss";
+
+
+// importing assets
+import pysznePl from "../../assets/pysznepl.png";
+import deliGoo from "../../assets/deligoo.png";
+import glovo from "../../assets/glovo.png";
+import uberEats from "../../assets/ubereats.png";
+import boltFood from "../../assets/boltfood.png";
+
+
+const LogoCarousel = () => {
+    return ( 
+        <div></div>
+     );
+}
+ 
+export default LogoCarousel;
