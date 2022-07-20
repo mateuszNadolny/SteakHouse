cf16f89606ed334420adbe7779deac82e6b3ad67
mateuszNadolny <mateusznadolny00@gmail.com>
2022-07-20 17:27:05 +0200

    beta version of logo carousel implemented

--- src/components/logoCarousel/LogoCarousel.js
+++ src/components/logoCarousel/LogoCarousel.js
@@ -1,26 +1,38 @@
 import { Autoplay } from 'swiper';
 import { Swiper, SwiperSlide }  from 'swiper/react';
 
+
 import "./../../global_styles/index.css";
 import styles from "./LogoCarousel.module.scss";
 import 'swiper/scss';
 
 
 // importing assets
-import pysznePl from "../../assets/pysznepl.png";
-import deliGoo from "../../assets/deligoo.png";
-import glovo from "../../assets/glovo.png";
-import uberEats from "../../assets/ubereats.png";
-import boltFood from "../../assets/boltfood.png";
+const pysznePl = "https://github.com/mateuszNadolny/SteakHouse/blob/25226a124d18ab2865c008a4bd87b134f2309703/src/assets/pysznepl.png";
+const deliGoo = "https://github.com/mateuszNadolny/SteakHouse/blob/25226a124d18ab2865c008a4bd87b134f2309703/src/assets/deligoo.png";
+const glovo = "https://github.com/mateuszNadolny/SteakHouse/blob/25226a124d18ab2865c008a4bd87b134f2309703/src/assets/glovo.png";
+const uberEats = "https://github.com/mateuszNadolny/SteakHouse/blob/25226a124d18ab2865c008a4bd87b134f2309703/src/assets/ubereats.png";
+const boltFood = "https://github.com/mateuszNadolny/SteakHouse/blob/25226a124d18ab2865c008a4bd87b134f2309703/src/assets/boltfood.png";
 
 
 const LogoCarousel = () => {
     const params = {
-        
+        effect: "slide",
+        // loop: true,
+        autoplay: {
+            delay: 1000,
+            disableOnInteraction: false,
+          },
+        // modules: [Autoplay],
+        style: {
+            zIndex: 3,
+        },
     }
 
     return ( 
-        <div></div>
+        <Swiper {...params}>  
+        </Swiper>
+        
      );
 }
  
